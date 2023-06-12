9e8973b10ba1f64150158a5fa0c88d5584a92175
Saransh Raina <saranshraina1@gmail.com>
2023-06-12 17:24:18 -0500

    early testing

--- alamo-frontend/src/App.js
+++ alamo-frontend/src/App.js
@@ -1,15 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import userService from './services/UserService'
 import './App.css';
 
-import userService from './services/UserService'
-
 const App = () => {
-  const [info, setInfo] = useState("")
+  const [mapATList, setMapATList] = useState([])
 
-  console.log(info)
+  useEffect(() => {
+    userService.getInfo()
+      .then(response => {
+        const mapDetails = response.campaigns[0].mapsDetail
+        let mapList = []
+        for (let i = 0; i < mapDetails.length; i++) {
+          const newMap = {
+            UID: mapDetails[i].mapUid,
+            AT: mapDetails[i].authorScore,
+            ATHolders: []
+          }
+          mapList.push(newMap)
+        }
+
+
+        const mapRecords = response.campaigns[0].mapsRecords
+        for (const map in mapRecords) {
+          const records = mapRecords[map].tops
+          const mapObj = mapList.find(elem => elem.UID === map)
+          for (let i = 0; i < records.length; i++) {
+            if (records[i].time <= mapObj.AT) {
+              mapObj.ATHolders.push(records[i].player.name)
+            }
+          }
+        }
+
+        setMapATList(mapList)
+      })
+  }, [])
+
+  const constructLeaderboard = () => {
+    let playerArr = []
+    console.log(mapATList)
+    for (let i = 0; i < mapATList.length; i++) {
+      const curMap = mapATList[i]
+      for (let j = 0; j < curMap.ATHolders.length; j++) {
+        let player = playerArr.find(player => player.name === curMap.ATHolders[j])
+        if (player) {
+          player.ATcount++
+        } else {
+          let newPlayer = {
+            name: curMap.ATHolders[j],
+            ATcount: 1
+          }
+          playerArr.push(newPlayer)
+        }
+      }
+    }
+    console.log(playerArr)
+  }
+
   return (
     <div>
-      Hello
+      {constructLeaderboard()}
     </div>
   )
 }
--- alamo-frontend/src/services/UserService.js
+++ alamo-frontend/src/services/UserService.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
-const baseURL = ''
+const baseURL = 'https://raw.githubusercontent.com/sRaina3/alamo-cup/main/alamo-backend/data.json'
 
-const getInfo = (info) => {
-
+const getInfo = () => {
   const request = axios.get(baseURL)
   return request.then(response => response.data)
 }
-
 const userService = {getInfo}
 
 export default userService
