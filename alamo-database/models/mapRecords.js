017caf99cee0790232e9ada1e80dc759a4e9597d
Saransh Raina <saranshraina1@gmail.com>
2023-07-25 00:37:28 -0500

    map record display update

---
+++ alamo-database/models/mapRecords.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const mapRecordsSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  authorName: String,
+  authorScore: Number,
+  records: Array
+})
+
+mapRecordsSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('MapRecords', mapRecordsSchema)
