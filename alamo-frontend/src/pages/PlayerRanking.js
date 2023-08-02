e0b24c3afdcecf718261fe6cd7a8fe19e441215e
Saransh Raina <saranshraina1@gmail.com>
2023-08-02 16:26:05 -0500

    add page change view to bottom of current page

--- alamo-frontend/src/pages/PlayerRanking.js
+++ alamo-frontend/src/pages/PlayerRanking.js
@@ -1,18 +1,42 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './PlayerRanking.css';
 
-const PlayerRanking = ({playerList, trackCount}) => {
-  const [curPage, setCurPage] = useState(1)
+const PLAYERS_PER_PAGE = 100; // Adjust this number as needed
 
-  let playerArr = []
-  const constructLeaderboard = () => {
-    for (let i = 0; i < playerList.length; i++) {
-      if (playerList[i].ATcount > 0) {
-        playerArr.push(playerList[i])
-      }
+const PlayerRanking = ({ playerList, trackCount }) => {
+  const [curPage, setCurPage] = useState(1);
+
+  const sortByAT = (a, b) => b.ATcount - a.ATcount;
+
+  const filteredAndSortedPlayers = playerList.filter(player => player.ATcount > 0).sort(sortByAT);
+
+  const totalPages = Math.ceil(filteredAndSortedPlayers.length / PLAYERS_PER_PAGE);
+  const startIndex = (curPage - 1) * PLAYERS_PER_PAGE;
+  const playersToShow = filteredAndSortedPlayers.slice(startIndex, startIndex + PLAYERS_PER_PAGE);
+
+  const prevPage = () => {
+    if (curPage > 1) {
+      setCurPage(curPage - 1);
     }
-    playerArr.sort(sortByAT)
-    return (
+  };
+
+  const nextPage = () => {
+    if (curPage < totalPages) {
+      setCurPage(curPage + 1);
+    }
+  };
+
+  return (
+    <div>
+      <div className="header-text">
+        <h1>AT Rankings</h1>
+      </div>
+
+      <div className="page-buttons-container">
+        <button className="page-button" onClick={prevPage}>&#9664; Prev</button>
+        <button className="page-button" onClick={nextPage}>Next {"\u25B6"}</button>
+      </div>
+
       <div className="leaderboard">
         <table>
           <thead>
@@ -24,61 +48,33 @@ const PlayerRanking = ({playerList, trackCount}) => {
             </tr>
           </thead>
           <tbody>
-            {displayLeaderboard(playerArr.slice(((curPage - 1) * 100), curPage * 100))}
+            {playersToShow.map((player, index) => (
+              <tr key={player.id}>
+                <td
+                  className={`rank ${
+                    (curPage === 1 && index === 0 && 'gold') ||
+                    (curPage === 1 && index === 1 && 'silver') ||
+                    (curPage === 1 && index === 2 && 'bronze') ||
+                    ''
+                  }`}
+                >
+                  {index + 1 + startIndex}
+                </td>
+                <td className="displayFont">{player.id}</td>
+                <td className="displayFont">{player.ATcount}</td>
+                <td className="displayFont">{trackCount - player.ATcount}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
-    )
-  }
 
-  const sortByAT = (a, b) => {
-    return b.ATcount - a.ATcount
-  }
-
-  const displayLeaderboard = (list) => {
-    return (
-      <>
-        {list.map((player, index) => (
-          <tr key={player.id}>
-            <td className={`rank ${curPage === 1 && index === 0 ? 'gold' : curPage === 1 
-                            && index === 1 ? 'silver' : curPage === 1 && index === 2 ? 'bronze' : ''}`}>
-              {index + 1 + ((curPage - 1) * 100)}
-            </td>
-            <td className='displayFont'>{player.id}</td>
-            <td className='displayFont'>{player.ATcount}</td>
-            <td className='displayFont'>{trackCount - player.ATcount}</td>
-          </tr>
-        ))}
-      </>
-    )
-  }
-
-  const prevPage = () => {
-    if (curPage > 1) {
-      setCurPage(curPage - 1)
-    }
-  }
-
-  const nextPage = () => {
-    if (curPage * 100 < playerList.length) {
-      setCurPage(curPage + 1)
-    }
-  }
-
-  return (
-    <div>
-      <div className='header-text'>
-        <h1>AT Rankings</h1>
+      <div className="page-buttons-container">
+        <button className="page-button" onClick={prevPage}>&#9664; Prev</button>
+        <button className="page-button" onClick={nextPage}>Next {"\u25B6"}</button>
       </div>
-      <div className="page-buttons-container-left">
-        <button className='page-button' onClick={prevPage}>&#9664; Prev</button>
-      </div>
-      <div className="page-buttons-container-right"> 
-        <button className='page-button' onClick={nextPage}>Next {"\u25B6"}</button>
-      </div>
-      {constructLeaderboard()}
     </div>
-  )
-}
+  );
+};
 
-export default PlayerRanking
+export default PlayerRanking;
